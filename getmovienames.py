ea96e41d134cfc41a9ab106d0948b350409b5f9e
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-04-06 14:11:29 -0700

    added documentation for the movie class

--- getmovienames.py
+++ getmovienames.py
@@ -3,7 +3,7 @@ import display_movie_website
 import media
 from media import Movie
 import os
-
+# get the JSON file to parse
 file_directory = os.getcwd() + "/movienames.json"
 
 # load the JSON data into module
@@ -13,4 +13,5 @@ with open(file_directory) as data_file:
 
 # loop for each movie details in the JSON list.
     movies = [Movie(**movie) for movie in data]
+# call function to display movie website.
     display_movie_website.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,14 +1,17 @@
 import webbrowser
 
 
+# class defining functions and attributes for movie website
 class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
+# attributes for movie info that will be fetched dynamically and used on our website
     def __init__(self, movie_title, movie_storyline, poster_image, movie_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = movie_url
 
+# play the youtube video URL for the movie
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
